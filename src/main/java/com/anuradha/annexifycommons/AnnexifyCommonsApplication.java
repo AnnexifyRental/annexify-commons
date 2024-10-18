340a5f87b943f9c9f9771d01f755704ace8ccc05
Anuradha Ranasinghe <anuranasinghe151@gmail.com>
2024-10-18 22:13:57 +0530

    Initial commit

---
+++ src/main/java/com/anuradha/annexifycommons/AnnexifyCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.anuradha.annexifycommons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnexifyCommonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnexifyCommonsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anuradha/annexifycommons/AnnexifyCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuradha.annexifycommons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnexifyCommonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
